94cdf50ed9f6307c7723d640e5535236e30db5f9
Simon <mj-pery@gmail.com>
2025-12-20 23:44:25 +0100

    feat: Ajouter la licence GPLv2 au plugin MJ Elementor Templates

--- mj-elementor-templates.php
+++ mj-elementor-templates.php
@@ -11,6 +11,8 @@
  * Requires at least: 5.8
  * Requires PHP: 7.4
  * Elementor tested up to: 3.33
+ * License:     GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  *
  * @package mj-elementor-templates
  */
